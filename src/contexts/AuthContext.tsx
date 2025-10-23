3e2a233932ffc3a735710fa1517eacad322451bf
Angel <angelherrarte3@gmail.com>
2025-10-22 21:26:28 -0600

    Add AuthContext for managing authentication state; includes session, user, loading status, and login status management.

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,25 @@
+import { Session } from '@supabase/supabase-js';
+import { createContext, useContext } from 'react';
+import { User, EmptyUser } from '../types';
+
+export type AuthData = {
+  session: Session | null;
+  user: User;
+  isLoading: boolean;
+  isLoggedIn: boolean;
+};
+
+export const AuthContext = createContext<AuthData>({
+  session: null,
+  user: EmptyUser,
+  isLoading: true,
+  isLoggedIn: false,
+});
+
+export const useAuthContext = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuthContext must be used within an AuthProvider');
+  }
+  return context;
+};
