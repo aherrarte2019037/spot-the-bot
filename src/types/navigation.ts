b80198426cf9b050a5d3591c42b9b1588ea243d9
Angel <angelherrarte3@gmail.com>
2025-10-25 19:57:20 -0600

    Add navigation types and routes for authentication and app navigation

---
+++ src/types/navigation.ts
@@ -0,0 +1,30 @@
+import type { StackScreenProps } from '@react-navigation/stack';
+
+export enum NavigationRoutes {
+  SignIn = 'SignIn',
+  SignUp = 'SignUp',
+  Onboarding = 'Onboarding',
+  Home = 'Home',
+  Game = 'Game',
+  Results = 'Results',
+}
+
+export type AuthStackParamList = {
+  [NavigationRoutes.SignIn]: undefined;
+  [NavigationRoutes.SignUp]: undefined;
+};
+
+export type AuthStackScreenProps<T extends keyof AuthStackParamList> =
+  StackScreenProps<AuthStackParamList, T>;
+
+export type AppStackParamList = {
+  [NavigationRoutes.Onboarding]: { userId: string };
+  [NavigationRoutes.Home]: undefined;
+  [NavigationRoutes.Game]: { gameId: string };
+  [NavigationRoutes.Results]: { gameId: string; score: number };
+};
+
+export type AppStackScreenProps<T extends keyof AppStackParamList> =
+  StackScreenProps<AppStackParamList, T>;
+
+export type RootStackParamList = AuthStackParamList & AppStackParamList;
